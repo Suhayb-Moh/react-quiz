3119d1d587e2f986c34dcde46db543f0877d5aa0
Suhayb Mohamed <suhaybmaxamed@gmail.com>
2023-07-17 11:25:16 +0300

    almost finished

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,10 @@ import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
 import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+
+const SEC_PER_QUESTION = 60;
 
 const initialState = {
   questions: [],
@@ -18,6 +22,7 @@ const initialState = {
   answer: null,
   points: 0,
   highScore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -34,7 +39,11 @@ function reducer(state, action) {
         status: "error",
       };
     case "start":
-      return { ...state, status: "active" };
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SEC_PER_QUESTION,
+      };
     case "newAnswer":
       const question = state.questions.at(state.index);
       return {
@@ -56,14 +65,22 @@ function reducer(state, action) {
       };
     case "restart":
       return { ...initialState, questions: state.questions, status: "ready" };
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
     default:
       throw new Error("Invalid action");
   }
 }
 
 const App = () => {
-  const [{ questions, status, index, answer, points, highScore }, dispatch] =
-    useReducer(reducer, initialState);
+  const [
+    { questions, status, index, answer, points, highScore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce(
@@ -103,12 +120,15 @@ const App = () => {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton
-              dispatch={dispatch}
-              answer={answer}
-              numQuestions={numQuestions}
-              index={index}
-            />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                numQuestions={numQuestions}
+                index={index}
+              />
+            </Footer>
           </>
         )}
 
---
+++ src/components/Timer.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+const Timer = ({ dispatch, secondsRemaining }) => {
+  const min = Math.floor(secondsRemaining / 60);
+  const sec = secondsRemaining % 60;
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      dispatch({ type: "tick" });
+    }, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [dispatch]);
+
+  return (
+    <div className="timer">
+      {min < 10 && "0"}
+      {min}:{sec < 10 && "0"}
+      {sec}
+    </div>
+  );
+};
+
+export default Timer;
